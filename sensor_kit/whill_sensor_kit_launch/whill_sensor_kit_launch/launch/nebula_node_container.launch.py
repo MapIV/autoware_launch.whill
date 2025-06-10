328f390307b91d967e0366eef1854a997e2bfdb3
shusei nishimura <shusei.nishimura@map4.jp>
2025-06-10 17:15:02 +0900

    remap ring_outlier_filter

--- sensor_kit/whill_sensor_kit_launch/whill_sensor_kit_launch/launch/nebula_node_container.launch.py
+++ sensor_kit/whill_sensor_kit_launch/whill_sensor_kit_launch/launch/nebula_node_container.launch.py
@@ -237,7 +237,7 @@ def launch_setup(context, *args, **kwargs):
             name="ring_outlier_filter",
             remappings=[
                 ("input", "rectified/pointcloud_ex"),
-                ("output", "pointcloud_before_sync"),
+                ("output", "/sensing/lidar/concatenated/pointcloud"),
             ],
             parameters=[ring_outlier_filter_node_param, ring_outlier_output_frame],
             extra_arguments=[{"use_intra_process_comms": LaunchConfiguration("use_intra_process")}],
